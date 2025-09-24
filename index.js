2521ea5dd3df8a63f6d0cd8f5a5dd92c6083d286
beni2408 <jascarbenish@gmail.com>
2025-09-24 10:48:23 +0530

    changed .gitignore

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import connectDB from "./src/config/db.js";
 import errorHandler from "./src/middlewares/errorHandler.js";
 import authRouter from "./src/routes/authRoutes.js";
 import serviceRouter from "./src/routes/serviceRoutes.js";
+import bookingRouter from "./src/routes/bookingRoutes.js";
+import adminRouter from "./src/routes/adminRoutes.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,6 +14,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/service", serviceRouter);
+app.use("/api/booking", bookingRouter);
+app.use("/api/admin", adminRouter);
 
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
